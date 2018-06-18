3affa37a5bb46a3eee369f6b828ffec626725cf6
Bobby Lucas <bobbylucas622@yahoo.com>
2018-06-18 20:46:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var addElementToBeginningofArray = chocolateBars.unshift("milky way");
+var destructiveAddElementToBeginningOfArray = chocolateBars.
